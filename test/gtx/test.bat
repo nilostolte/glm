3f1bb463439edabed9294cdf005af4d429072122
Dr Nilo Stolte <80269251+nilostolte@users.noreply.github.com>
2023-04-12 16:27:27 -0400

    Add files via upload

---
+++ test/gtx/test.bat
@@ -0,0 +1,176 @@
+@echo off
+ECHO RUNNING TESTS
+ECHO Testing gtx
+"./gtx"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx: %ERRORLEVEL%
+ECHO Testing gtx_associated_min_max
+"./gtx_associated_min_max"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_associated_min_max: %ERRORLEVEL%
+ECHO Testing gtx_closest_point
+"./gtx_closest_point"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_closest_point: %ERRORLEVEL%
+ECHO Testing gtx_color_encoding
+"./gtx_color_encoding"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_color_encoding: %ERRORLEVEL%
+ECHO Testing gtx_color_space_YCoCg
+"./gtx_color_space_YCoCg"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_color_space_YCoCg: %ERRORLEVEL%
+ECHO Testing gtx_color_space
+"./gtx_color_space"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_color_space: %ERRORLEVEL%
+ECHO Testing gtx_common
+"./gtx_common"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_common: %ERRORLEVEL%
+ECHO Testing gtx_compatibility
+"./gtx_compatibility"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_compatibility: %ERRORLEVEL%
+ECHO Testing gtx_component_wise
+"./gtx_component_wise"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_component_wise: %ERRORLEVEL%
+ECHO Testing gtx_easing
+"./gtx_easing"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_easing: %ERRORLEVEL%
+ECHO Testing gtx_euler_angle
+"./gtx_euler_angle"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_euler_angle: %ERRORLEVEL%
+ECHO Testing gtx_extend
+"./gtx_extend"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_extend: %ERRORLEVEL%
+ECHO Testing gtx_extended_min_max
+"./gtx_extended_min_max"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_extended_min_max: %ERRORLEVEL%
+ECHO Testing gtx_exterior_product
+"./gtx_exterior_product"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_exterior_product: %ERRORLEVEL%
+ECHO Testing gtx_fast_exponential
+"./gtx_fast_exponential"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_fast_exponential: %ERRORLEVEL%
+ECHO Testing gtx_fast_square_root
+"./gtx_fast_square_root"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_fast_square_root: %ERRORLEVEL%
+ECHO Testing gtx_fast_trigonometry
+"./gtx_fast_trigonometry"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_fast_trigonometry: %ERRORLEVEL%
+ECHO Testing gtx_functions
+"./gtx_functions"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_functions: %ERRORLEVEL%
+ECHO Testing gtx_gradient_paint
+"./gtx_gradient_paint"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_gradient_paint: %ERRORLEVEL%
+ECHO Testing gtx_handed_coordinate_space
+"./gtx_handed_coordinate_space"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_handed_coordinate_space: %ERRORLEVEL%
+ECHO Testing gtx_integer
+"./gtx_integer"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_integer: %ERRORLEVEL%
+ECHO Testing gtx_intersect
+"./gtx_intersect"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_intersect: %ERRORLEVEL%
+ECHO Testing gtx_io
+"./gtx_io"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_io: %ERRORLEVEL%
+ECHO Testing gtx_load
+"./gtx_load"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_load: %ERRORLEVEL%
+ECHO Testing gtx_log_base
+"./gtx_log_base"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_log_base: %ERRORLEVEL%
+ECHO Testing gtx_matrix_cross_product
+"./gtx_matrix_cross_product"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_cross_product: %ERRORLEVEL%
+ECHO Testing gtx_matrix_decompose
+"./gtx_matrix_decompose"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_decompose: %ERRORLEVEL%
+ECHO Testing gtx_matrix_factorisation
+"./gtx_matrix_factorisation"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_factorisation: %ERRORLEVEL%
+ECHO Testing gtx_matrix_interpolation
+"./gtx_matrix_interpolation"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_interpolation: %ERRORLEVEL%
+ECHO Testing gtx_matrix_major_storage
+"./gtx_matrix_major_storage"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_major_storage: %ERRORLEVEL%
+ECHO Testing gtx_matrix_operation
+"./gtx_matrix_operation"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_operation: %ERRORLEVEL%
+ECHO Testing gtx_matrix_query
+"./gtx_matrix_query"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_query: %ERRORLEVEL%
+ECHO Testing gtx_matrix_transform_2d
+"./gtx_matrix_transform_2d"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_matrix_transform_2d: %ERRORLEVEL%
+ECHO Testing gtx_norm
+"./gtx_norm"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_norm: %ERRORLEVEL%
+ECHO Testing gtx_normal
+"./gtx_normal"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_normal: %ERRORLEVEL%
+ECHO Testing gtx_normalize_dot
+"./gtx_normalize_dot"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_normalize_dot: %ERRORLEVEL%
+ECHO Testing gtx_number_precision
+"./gtx_number_precision"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_number_precision: %ERRORLEVEL%
+ECHO Testing gtx_orthonormalize
+"./gtx_orthonormalize"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_orthonormalize: %ERRORLEVEL%
+ECHO Testing gtx_optimum_pow
+"./gtx_optimum_pow"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_optimum_pow: %ERRORLEVEL%
+ECHO Testing gtx_perpendicular
+"./gtx_perpendicular"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_perpendicular: %ERRORLEVEL%
+ECHO Testing gtx_polar_coordinates
+"./gtx_polar_coordinates"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_polar_coordinates: %ERRORLEVEL%
+ECHO Testing gtx_projection
+"./gtx_projection"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_projection: %ERRORLEVEL%
+ECHO Testing gtx_quaternion
+"./gtx_quaternion"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_quaternion: %ERRORLEVEL%
+ECHO Testing gtx_dual_quaternion
+"./gtx_dual_quaternion"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_dual_quaternion: %ERRORLEVEL%
+ECHO Testing gtx_range
+"./gtx_range"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_range: %ERRORLEVEL%
+ECHO Testing gtx_rotate_normalized_axis
+"./gtx_rotate_normalized_axis"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_rotate_normalized_axis: %ERRORLEVEL%
+ECHO Testing gtx_rotate_vector
+"./gtx_rotate_vector"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_rotate_vector: %ERRORLEVEL%
+ECHO Testing gtx_scalar_multiplication
+"./gtx_scalar_multiplication"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_scalar_multiplication: %ERRORLEVEL%
+ECHO Testing gtx_scalar_relational
+"./gtx_scalar_relational"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_scalar_relational: %ERRORLEVEL%
+ECHO Testing gtx_spline
+"./gtx_spline"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_spline: %ERRORLEVEL%
+ECHO Testing gtx_string_cast
+"./gtx_string_cast"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_string_cast: %ERRORLEVEL%
+ECHO Testing gtx_texture
+"./gtx_texture"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_texture: %ERRORLEVEL%
+ECHO Testing gtx_type_aligned
+"./gtx_type_aligned"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_type_aligned: %ERRORLEVEL%
+ECHO Testing gtx_type_trait
+"./gtx_type_trait"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_type_trait: %ERRORLEVEL%
+ECHO Testing gtx_vec_swizzle
+"./gtx_vec_swizzle"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_vec_swizzle: %ERRORLEVEL%
+ECHO Testing gtx_vector_angle
+"./gtx_vector_angle"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_vector_angle: %ERRORLEVEL%
+ECHO Testing gtx_vector_query
+"./gtx_vector_query"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_vector_query: %ERRORLEVEL%
+ECHO Testing gtx_wrap
+"./gtx_wrap"
+IF %ERRORLEVEL% NEQ 0 ECHO gtx_wrap: %ERRORLEVEL%
